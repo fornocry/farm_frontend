21d391216f4a92df1763f9428ee31b5723f7a7c5
user <asdasd@gmail.com>
2024-09-28 23:23:24 +0300

    kostil api url)

--- src/features/api/index.ts
+++ src/features/api/index.ts
@@ -6,7 +6,6 @@ if (import.meta.env.DEV) {
     console.log(`NODE_ENV is dev`);
     axios.defaults.baseURL = 'http://localhost:8001'
 } else if (import.meta.env.PROD) {
-    console.log('NODE_ENV is production');
-    console.log(`API url https://api.gentelmanclub.pro`)
+    console.log('production');
     axios.defaults.baseURL = "https://api.gentelmanclub.pro"
 }
